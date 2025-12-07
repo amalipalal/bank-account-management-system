6ba3062e30094915227bad951d4d7ae207361925
amalipalal <palal.asare@amalitech.com>
2025-12-07 09:11:21 +0000

    update id generation to Transaction creation (#10)
    * add transactionId to Transaction model dependencies
    
    * add TransactionIdGenerator for transaction models
    
    * add AutoIdGenerator as dependency of TransactionManager
    
    - adds a createTransaction method
    - adds a constructor variable
    
    * update transaction processing to use createTransaction method
    
    * update bankingService instantiation

---
+++ src/main/java/utils/id/TransactionIdGenerator.java
@@ -0,0 +1,36 @@
+package utils.id;
+
+import interfaces.AutoIdGenerator;
+
+import java.text.DecimalFormat;
+
+public class TransactionIdGenerator implements AutoIdGenerator {
+    private final DecimalFormat FORMATTER = new DecimalFormat("000");
+    private int transactionCounter;
+
+    public TransactionIdGenerator() { this.transactionCounter = 0; }
+
+    @Override
+    public String generateId() {
+        this.transactionCounter += 1;
+        String idString = FORMATTER.format(transactionCounter);
+        return "TXN" + idString;
+    }
+
+    @Override
+    public int getCounter() {
+        return this.transactionCounter;
+    }
+
+    @Override
+    public int extractIndex(String transactionNumber) {
+        int startIndex = transactionNumber.length() - 3;
+        String indexString = transactionNumber.substring(startIndex);
+
+        try {
+            return Integer.parseInt(indexString) - 1;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Transaction number format is invalid: " + transactionNumber);
+        }
+    }
+}
